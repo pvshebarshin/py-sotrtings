57d85893c06820778d633b4ad61e05e9744a5d0c
pvshebarshin <pvshebarshin@edu.hse.ru>
2024-11-19 23:59:27 +0300

    Add Heap Sort

--- src/python/sortings.py
+++ src/python/sortings.py
@@ -164,3 +164,35 @@ def __merge_sort(arr):
             arr[k] = right_half[j]
             j += 1
             k += 1
+
+
+def __heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        __heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        __heapify(arr, i, 0)
+
+
+def __heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        __heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    __heap_sort(arr)
+    return arr
